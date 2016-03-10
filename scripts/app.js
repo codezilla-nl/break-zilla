7da2126abd84f349986431de6b784d3bd59d86b4
Remidy <remiham@gmail.com>
2016-03-10 11:12:30 +0100

    Black screen

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,9 @@ MY.App = function () {};
 
 MY.App.prototype = {
 	init: function (canvas) {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+
 		var game = new MY.Game();
         game.init(canvas);
 	}
